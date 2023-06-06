e5d1ab7a476c660bbb9e4dcbcd029153ba60da3a
brf153 <153hsb@gmail.com>
2023-06-06 12:02:20 +0530

    Revert "done"
    This reverts commit 36a73ffb8f05e6ea474c633837f9985ed660c0ee.

--- Backend/app.js
+++ Backend/app.js
@@ -40,9 +40,9 @@ app.use("/api/v1",payment)
 app.use("/api/v1/",cart)
 app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-// app.get("*",(req,res)=>{
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
-// })
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+})
 
 //Middleware for Errors
 app.use(errorMiddleware);
